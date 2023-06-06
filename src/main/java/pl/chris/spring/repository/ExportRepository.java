f5b758bfb4062cc8de6cf731ee41a9b87ed6eec6
Krzysztof Telka <krzysztof.telka@kyriba.com>
2023-06-06 12:12:39 +0200

    Changed port number + optimistic lock

--- src/main/java/pl/chris/spring/repository/ExportRepository.java
+++ src/main/java/pl/chris/spring/repository/ExportRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.transaction.annotation.Transactional;
 import pl.chris.spring.entity.ExportEntity;
 
 public interface ExportRepository extends JpaRepository<ExportEntity, UUID> {
-
+    @Transactional(readOnly = true)
     Page<ExportEntity> findAllByDomainUuid(String domainUuid, Pageable pageable);
 }
